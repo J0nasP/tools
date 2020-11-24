37e2b554a8f7ab238b4a84c0e81de7d8460866f8
J0nasP <petersen0108@gmail.com>
2020-11-24 11:53:00 +0100

    set up file edited

--- setup.py
+++ setup.py
@@ -2,3 +2,13 @@ from distutils.core import setup
 import py2exe
 
 
+setup(
+        windows=['Toolbox.py'],
+        options={
+                "py2exe":{
+                        "unbuffered": True,
+                        "optimize": 2,
+                        "includes": ['lxml.etree', 'lxml._elementpath', 'gzip']
+                }
+        }
+)
